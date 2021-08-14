0a6c199305ae96065d03f27cd6afff21ba816169
oren-BA <orenbenaltabe@gmail.com>
2021-08-14 14:05:36 +0300

    implemented validate token

--- structs/LineOfCode.c
+++ structs/LineOfCode.c
@@ -12,37 +12,37 @@
 #define I_COMMANDS_NUM 15
 #define J_COMMANDS_NUM 4
 
-char** splitLine(char* sourceCode, int wordNum){
+char **splitLine(char *sourceCode, int wordNum) {
     int end = 0;
     int start = 0;
-    char** words = (char**)malloc(sizeof(char*) * wordNum);
+    char **words = malloc(sizeof(char *) * wordNum);
     int wordIndex = 0;
     int wordLen;
-    while (sourceCode[end] != '\n'){
+    while (sourceCode[end] != '\n') {
         /*TODO add support for consecutive whitespaces*/
-        if (sourceCode[end] == ' ' || sourceCode[end] == ','){
-            wordLen = end-start;
+        if (sourceCode[end] == ' ' || sourceCode[end] == ',') {
+            wordLen = end - start;
             /* +1 for null terminator */
-            words[wordIndex] = malloc(sizeof(char)*(wordLen+1));
-            memcpy(words[wordIndex], sourceCode+start, wordLen );
+            words[wordIndex] = malloc(sizeof(char) * (wordLen + 1));
+            memcpy(words[wordIndex], sourceCode + start, wordLen);
             words[wordIndex][wordLen] = '\0';
-            start = end+1;
+            start = end + 1;
             end++;
             wordIndex++;
         }
         end++;
     }
     /* Handle last word */
-    wordLen = end-start;
-    words[wordIndex] = malloc(sizeof(char)*(wordLen+1)); /* +1 for null terminator */
-    memcpy(words[wordIndex], sourceCode+start, wordLen );
+    wordLen = end - start;
+    words[wordIndex] = malloc(sizeof(char) * (wordLen + 1)); /* +1 for null terminator */
+    memcpy(words[wordIndex], sourceCode + start, wordLen);
     words[wordIndex][wordLen] = '\0';
     return words;
 }
 
-Token * tokenize(char* sourceCode, int tokenNum){
-    char** words = splitLine(sourceCode, tokenNum);
-    Token* tokens = (Token*)malloc(sizeof(Token)*tokenNum);
+Token *tokenize(char *sourceCode, int tokenNum) {
+    char **words = splitLine(sourceCode, tokenNum);
+    Token *tokens = (Token *) malloc(sizeof(Token) * tokenNum);
     int i;
     for (i = 0; i < tokenNum; ++i) {
         tokens[i] = wordToToken(words[i]);
@@ -50,97 +50,124 @@ Token * tokenize(char* sourceCode, int tokenNum){
     return tokens;
 }
 
-int tokenCount(const char* sourceCode){
+int tokenCount(const char *sourceCode) {
     int i = 0;
     int counter = 0;
     /* counter seperators */
-    while (sourceCode[i] != '\n'){
-        if(sourceCode[i] == ' ' || sourceCode[i] == ','){
+    while (sourceCode[i] != '\n') {
+        if (sourceCode[i] == ' ' || sourceCode[i] == ',') {
             counter++;
         }
         i++;
     }
-    return counter+1;
+    return counter + 1;
 }
 
-BinaryCommand tokensToBinary(Token* tokens){
+BinaryCommand tokensToBinary(Token *tokens) {
     /* TODO  implement*/
-    BinaryCommand* b = (BinaryCommand*)malloc(1);
+    BinaryCommand *b = (BinaryCommand *) malloc(1);
     return *b;
 }
 
 
-
-bool validateLabelDef(char* w){
+int validateLabelDef(char *w) {
     /*TODO*/
+    return TRUE;
 }
 
-enum CommandType getCommandType(char* cmd){
+enum CommandType getCommandType(char *cmd) {
     /*TODO*/
     int i;
-    char* rCommands[] = {"add", "sub", "and", "or", "nor", "move", "mhvi", "mvlo"};
-    char* jCommands[] = {"jmp", "la", "call","stop"};
+    char *rCommands[] = {"add", "sub", "and", "or", "nor", "move", "mhvi", "mvlo"};
+    char *jCommands[] = {"jmp", "la", "call", "stop"};
     for (i = 0; i < R_COMMANDS_NUM; ++i) {
-        if (strcmp(cmd, rCommands[i]) == 0){
+        if (strcmp(cmd, rCommands[i]) == 0) {
             return R;
         }
     }
     for (i = 0; i < J_COMMANDS_NUM; ++i) {
-        if (strcmp(cmd, jCommands[i]) == 0){
+        if (strcmp(cmd, jCommands[i]) == 0) {
             return J;
         }
     }
     return I;
 }
 
-int isNum(char c){
+int isDigit(char c) {
     if ((c >= '0' && c <= '9')) return TRUE;
     return FALSE;
 }
 
-int validateRegister(char* text){
+int validateTypeOrder(Token *tokens, int token_num, const enum TokenType *types, int types_num) {
+    int i;
+    if (token_num != types_num) return FALSE;
+    for (i = 0; i < token_num; i++) {
+        if (tokens[i].type != types[i]) return FALSE;
+    }
+    return TRUE;
+}
+
+int validateRegister(char *text) {
+    int regNum;
     int len = strlen(text);
     if (len != 2 && len != 3) return FALSE;
     if (text[0] != '$') return FALSE;
-    if (len == 2 && isNum(text[1]) == FALSE) return FALSE;
-    if (len == 3 && isNum(text[1]) == FALSE && isNum(text[2]) == FALSE) return FALSE;
-    int regNum = strtol(text+1, NULL,10);
-    if (regNum>31) return FALSE;
+    if (len == 2 && isDigit(text[1]) == FALSE) return FALSE;
+    if (len == 3 && isDigit(text[1]) == FALSE && isDigit(text[2]) == FALSE) return FALSE;
+    regNum = strtol(text + 1, NULL, 10);
+    if (regNum > 31) return FALSE;
     return TRUE;
 }
 
-int validateR(LineOfCode line){
-    int i = 1;
-    if (line.tokens[0].type == LabelDefinition){
-        i = 2;
-    }
-    for (; i <line.tokens_num; ++i) { /*check if the last three tokens are valid registers*/
-        if(line.tokens[i].type != Register || validateRegister(line.tokens[i].content) == FALSE) return FALSE;
-    }
+int validateToken(Token t){
+    if (strlen(t.content) == 0) return FALSE;
+    if (t.type == Register) return validateRegister(t.content);
     return TRUE;
 }
 
-int validateI(LineOfCode line){
-    /*TODO*/
+int validateR(LineOfCode line) {
+    enum TokenType types[4] = {Command, Register, Register, Register};
+    if (line.tokens_num != 4) return FALSE; /*checks if line has 4 tokens */
+    if (validateTypeOrder(line.tokens, line.tokens_num, types,4)) return TRUE;
+    return FALSE;
 }
 
-int validateJ(LineOfCode line){
-    /*TODO*/
+int isConditionBranch(char *cmd) {
+
 }
 
 
-
-int validate_line(LineOfCode line){
-    int i = 0;
-    if (line.tokens[i].type == LabelDefinition){
-        if (validateLabelDef(line.tokens[i].content) == FALSE) return FALSE;
-        i++;
+int validateI(LineOfCode line) {
+    /*TODO*/
+    Token *tokens = line.tokens;
+    enum TokenType types[4] = {Command, Register, Register, Label};
+    enum TokenType types2[4] = {Command, Register, Number, Register};
+    if (line.tokens_num != 4) return FALSE; /*checks if line has 4 tokens*/
+    if (isConditionBranch(tokens[0].content) ==
+        TRUE) {     /*checks if command is in the condition branch format which is: cmd, reg, reg, label*/
+        if (validateTypeOrder(tokens, line.tokens_num, types, 4)) return TRUE;
+    } else {     /*checks if command is in the other format which is: cmd, reg, num, reg*/
+        if (validateTypeOrder(tokens, line.tokens_num, types2, 4)) {
+            return TRUE;
+        }
     }
-    if (isCommandWord(line.tokens[i].content) == TRUE){
-        enum CommandType commandType = getCommandType(line.tokens[i].content);
-        if (commandType == R){
+    return FALSE;
+}
+
+int validateJ(LineOfCode line) {
+    /*TODO*/
+}
+
+
+int validate_line(LineOfCode line) {
+    if (line.has_label) {
+        if (validateLabelDef(line.label.content) == FALSE) return FALSE;
+    }
+    if (isCommandWord(line.tokens[0].content) == TRUE) {
+        enum CommandType commandType = getCommandType(line.tokens[0].content);
+        if (commandType == R) {
             return validateR(line);
-        } else if (commandType == I){
+        } else if (commandType == I) {
             return validateI(line);
         }
         return validateJ(line);
@@ -149,11 +176,17 @@ int validate_line(LineOfCode line){
 
 }
 
-LineOfCode* createLine(char* sourceCode, int address){
+LineOfCode *createLine(char *sourceCode, int address) {
     LineOfCode *l = malloc(sizeof(l));
     l->source = sourceCode;
     l->tokens_num = tokenCount(sourceCode);
-    l->tokens = tokenize(sourceCode, l->tokens_num);
+    l->has_label = FALSE;
+    l->tokens = tokenize(sourceCode, l->tokens_num);;
+    if (l->tokens[0].type == LabelDefinition) {
+        l->has_label = TRUE;
+        l->label = l->tokens[0];
+        ++l->tokens;
+    }
     l->address = address;
     l->binary_command = tokensToBinary(l->tokens);
     return l;
