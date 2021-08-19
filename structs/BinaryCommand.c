55b27e37d500ae6872cc8d272bef553f0bd67199
Unknown <tomerbenaltabe@gmail.com>
2021-08-20 00:44:11 +0300

    allocated payload

--- structs/BinaryCommand.c
+++ structs/BinaryCommand.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
+#include <memory.h>
 #include "BinaryCommand.h"
 
 BinaryCommand *createBinary(size_t size, char* payload, char* mask){
     BinaryCommand* command_ptr = malloc(sizeof(BinaryCommand));
-    command_ptr->payload = payload;
+    command_ptr->payload = malloc(size);
+    memcpy(command_ptr->payload, payload, size);
     command_ptr->mask = mask;
     command_ptr->size = size;
     return command_ptr;
